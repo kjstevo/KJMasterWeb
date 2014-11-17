a1712a63720e43ee3488999c431c3ce6078bc727
Steven Dees <kjstevokjmaster@gmail.com>
2014-11-17 15:07:09 -0800

    Fixed sign up redirect

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -1,42 +1,48 @@
 'use strict';
 
 angular.module('kjmApp')
-  .config(function($stateProvider) {
-    $stateProvider
-      .state('login', {
-        url: '/login',
-        templateUrl: 'app/account/login/login.html',
-        controller: 'LoginCtrl'
-      })
-      .state('logout', {
-        url: '/logout?referrer',
-        referrer: 'main',
-        template: '',
-        controller: function($state, AuthDb) {
-          var referrer = $state.params.referrer ||
-                          $state.current.referrer ||
-                          'main';
-          AuthDb.logout();
-          $state.go(referrer);
-          
-        }
-      })
-      .state('signup', {
-        url: '/signup',
-        templateUrl: 'app/account/signup/signup.html',
-        controller: 'SignupCtrl'
-      })
-      .state('settings', {
-        url: '/settings',
-        templateUrl: 'app/account/settings/settings.html',
-        controller: 'SettingsCtrl',
-        authenticate: true
-      });
-  })
-  .run(function($rootScope) {
-    $rootScope.$on('$stateChangeStart', function(event, next, nextParams, current) {
-      if (next.name === 'logout' && current && current.name && !current.authenticate) {
-        next.referrer = current.name;
-      }
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'app/account/login/login.html',
+                controller: 'LoginCtrl'
+            })
+            .state('logout', {
+                url: '/logout?referrer',
+                referrer: 'main',
+                template: '',
+                controller: function($state, AuthDb) {
+                    var referrer = $state.params.referrer ||
+                        $state.current.referrer ||
+                        'main';
+                    AuthDb.logout().then(function() {
+                            $state.go(referrer);
+                        },
+
+                        function(err) {
+                            console.log(err);
+                            $state.go(referrer);
+                        });
+
+                }
+            })
+            .state('signup', {
+                url: '/signup',
+                templateUrl: 'app/account/signup/signup.html',
+                controller: 'SignupCtrl'
+            })
+            .state('settings', {
+                url: '/settings',
+                templateUrl: 'app/account/settings/settings.html',
+                controller: 'SettingsCtrl',
+                authenticate: true
+            });
+    })
+    .run(function($rootScope) {
+        $rootScope.$on('$stateChangeStart', function(event, next, nextParams, current) {
+            if (next.name === 'logout' && current && current.name && !current.authenticate) {
+                next.referrer = current.name;
+            }
+        });
     });
-  });
--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -1,55 +1,59 @@
 'use strict';
 
 angular.module('kjmApp')
-  .controller('SignupCtrl', function($scope, AuthDb, $state, $window) {
-    $scope.user = {};
-    $scope.errors = {};
+    .controller('SignupCtrl', function($scope, AuthDb, $state, $window) {
+        $scope.user = {};
+        $scope.errors = {};
 
-    $scope.register = function(form) {
-      $scope.submitted = true;
+        $scope.register = function(form) {
+            $scope.submitted = true;
 
-      if (form.$valid) {
-  //       Auth.createUser($scope.user.nick,$scope.user.email,$scope.user.password
-  //       ,{
-  //         success: function() {
-  //         // Account created, redirect to home
-  //         $state.go('main');
-  //          },
-  // error:  function(err) {
-  //         err = err.data;
-  //         $scope.errors = {};
-  //     // Update validity of form fields that match the mongoose errors
-  //         angular.forEach(err.errors, function(error, field) {
-  //           form[field].$setValidity('mongoose', false);
-  //           $scope.errors[field] = error.message;
-  //         });
-  //       }});
-    AuthDb.createUser($scope.user.email, $scope.user.password,
-      {
-      email:   $scope.user.email,
-      nick: $scope.user.nick,
-      role: 'user'
-    },{
-        success: function() {
-            // Account created, redirect to home
-          // AuthDb.login($scope.user.email,$scope.user.password);
-            $state.go('main');
-             },
-        error:  function(err) {
-          alert(err);
-            err = err.data;
-            $scope.errors = {};
-        // Update validity of form fields that match the mongoose errors
-           // angular.forEach(err.errors, function(error, field) {
-         console.log(err.data);
-         }});
-        
-          //end signup
+            if (form.$valid) {
 
-      }//endif
-    };  //end func
+                AuthDb.createUser($scope.user.email, $scope.user.password, {
+                    email: $scope.user.email,
+                    nick: $scope.user.nick,
+                    role: 'user'
+                }).then(function() {
+                        // Account created, redirect to home
+                        AuthDb.login($scope.user.email, $scope.user.password).then(function() {
+                            $state.go('main');
+                        }, function(err) {
+                            $scope.addAlert('There was an error signing up', 'danger');
+                            console.log(err);
+                        });
 
-    $scope.loginOauth = function(provider) {
-      $window.location.href = '/auth/' + provider;
-    };
-  });
+
+                    },
+                    function(err) {
+                        $scope.addAlert('There was an error signing up', 'danger');
+                        console.log(err);
+                        // Update validity of form fields that match the mongoose errors
+                        // angular.forEach(err.errors, function(error, field) {
+                    });
+
+                //end signup
+
+            } //endif
+        }; //end func
+
+        $scope.loginOauth = function(provider) {
+            $window.location.href = '/auth/' + provider;
+        };
+
+        //       Auth.createUser($scope.user.nick,$scope.user.email,$scope.user.password
+        //       ,{
+        //         success: function() {
+        //         // Account created, redirect to home
+        //         $state.go('main');
+        //          },
+        // error:  function(err) {
+        //         err = err.data;
+        //         $scope.errors = {};
+        //     // Update validity of form fields that match the mongoose errors
+        //         angular.forEach(err.errors, function(error, field) {
+        //           form[field].$setValidity('mongoose', false);
+        //           $scope.errors[field] = error.message;
+        //         });
+        //       }});
+    });
